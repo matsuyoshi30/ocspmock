8debfa519d56534b11ce9bf06cb3562c5f641c93
matsuyoshi30 <sfbgwm30@gmail.com>
2022-11-19 21:38:21 +0900

    randomize OCSP Response status

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"crypto/x509"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
 
 	"golang.org/x/crypto/ocsp"
+
+	"github.com/matsuyoshi30/ocspmock"
 )
 
 var status = []string{"Good", "Revoked", "Unknown"}
 
 func main() {
-	resp, err := http.Get("http://localhost:8080")
+	leafCert, _ := hex.DecodeString(ocspmock.LeafCertHex)
+	leaf, err := x509.ParseCertificate(leafCert)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	issuerCert, _ := hex.DecodeString(ocspmock.IssuerCertHex)
+	issuer, err := x509.ParseCertificate(issuerCert)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ocspReqBytes, err := ocsp.CreateRequest(leaf, issuer, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	resp, err := http.Post("http://localhost:8080", "application/ocsp-request", bytes.NewBuffer(ocspReqBytes))
 	if err != nil {
 		fmt.Println(err)
 		return
